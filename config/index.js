b4b9e7dc7b9e569920e9a118030f9641c88da02d
Toyosi Dairo <you@example.com>
2022-02-03 10:18:41 +0100

    Production version

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "myweb.com";
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://toyosi-vert.vercel.app/";
